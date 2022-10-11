3aae450354650ffdd168963c312eced8c4640c05
suhaakarthi <suhaa.cheran@gmail.com>
2022-10-11 19:19:12 +0530

    Customer.java

---
+++ Customer.java
@@ -0,0 +1,12 @@
+class Customer
+    {
+      public static void main(String[] args)
+        {
+            Theatre1 theatre = new Theatre1(); 
+             int amount=200;   
+            theatre.bookTicket(200);
+         }
+    }
+
+
+ 
