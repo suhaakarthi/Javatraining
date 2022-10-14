9845d40dfb724a26bd48c29c3af629c6683b3509
suhaakarthi <suhaa.cheran@gmail.com>
2022-10-14 22:14:07 +0530

    Employee2.java

---
+++ Employee2.java
@@ -0,0 +1,18 @@
+package tamilnadu.thiruvannamalai; 
+import tamilnadu.chennai.Office; 
+
+
+class Employee2
+{//Compile: 
+//javac -d . Employee2.java
+//Run: 
+//java tamilnadu.thiruvannamalai.Employee2
+public static void main(String[] args)
+{
+Office office = new Office();
+//Office.class  
+office.work(); 
+office.pay_salary(); 
+
+}
+}
