91110d97c466a3bbbca5e85a3dbe796692fceb7b
suhaakarthi <suhaa.cheran@gmail.com>
2022-10-14 22:12:15 +0530

    Employee1.java

---
+++ Employee1.java
@@ -0,0 +1,18 @@
+package tamilnadu.chennai; 
+//javac -d . Employee1.java
+//Run: 
+// .java file or .class file
+//
+//
+//java tamilnadu.chennai.Employee1
+class Employee1
+{
+public static void main(String[] args)
+{
+Office office = new Office(); 
+office.work(); 
+office.pay_salary(); 
+
+}
+
+}
