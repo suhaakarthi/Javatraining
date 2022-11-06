66d075ba2dca86cb345575caf913ee00972d8022
suhaakarthi <suhaa.cheran@gmail.com>
2022-11-06 18:45:36 +0530

    reverseOrder.java

---
+++ reverseOrder.java
@@ -0,0 +1,19 @@
+package practise;
+
+public class reverseOrder 
+{
+	public static void main(String[] args) 
+	{
+int no=1234;
+int rev=0;
+{
+	while (no>0) 
+	{
+	int rem=no%10;
+	rev=(rev*10)+rem;
+	no=no/10;
+	System.out.println(rev);
+	}
+}
+}
+}
