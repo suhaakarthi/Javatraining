47752f1a2cae232f03a11f552b29edf803cf254d
suhaakarthi <suhaa.cheran@gmail.com>
2022-11-18 12:14:24 +0530

    Printp2.java

---
+++ Printp2.java
@@ -0,0 +1,23 @@
+package Add;
+
+public class Printp2 
+ {
+	public static void main(String[] args)
+	{
+	int row=5;
+	while(row>=1)
+	{
+		for(int col=row;col>=1;col--)
+		{
+			System.out.print(col+" ");
+		}
+		System.out.println();
+		row--;
+	}
+	}
+ }
+
+
+	
+
+
